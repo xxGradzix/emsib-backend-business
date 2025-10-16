497bc40c0b8b2b3ba8440e6285c971d2cbb6a4ba
JakubGrad <xxGradzix@gmail.com>
2025-10-16 13:33:30 +0200

    basic project structure

---
+++ src/main/java/com/emsib/emsib_backend_business/EmsibBackendBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.emsib.emsib_backend_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsibBackendBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsibBackendBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emsib/emsib_backend_business/EmsibBackendBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsib.emsib_backend_business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsibBackendBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
